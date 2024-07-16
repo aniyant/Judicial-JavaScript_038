cb23e44d1c6bec0ef98264387ca92ef1150ae0c0
aniyant <kumarsunny30066@gmail.com>
2024-07-17 00:15:48 +0530

    backend basic setup: file structure,configuration,database

---
+++ backend/src/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const fileRoutes = require('./routes/fileRoutes');
+const codeExecutionRoutes = require('./routes/codeExecutionRoutes');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+// app.use('/api/auth', authRoutes);
+// app.use('/api/files', fileRoutes);
+// app.use('/api/code', codeExecutionRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+const PORT = process.env.PORT || 4500;
+
+app.listen(PORT, () => {
+    // database connection
+    connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
