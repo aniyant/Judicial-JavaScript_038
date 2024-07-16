c59d714dc5d0a0256baad9200b20a364e2c0efdb
aniyant <kumarsunny30066@gmail.com>
2024-07-17 00:53:36 +0530

    frontend in react: file structure,basic configuration done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Editor from './pages/Editor';
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/editor" element={<Editor />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App
