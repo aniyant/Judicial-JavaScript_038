b42c1f08048c801e8ae4232a272a01696e05e2ca
RSarvjyoti <sarvjyoti64@gmail.com>
2024-07-18 18:14:20 +0530

    Designed the login and signup page

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -2,6 +2,15 @@ import { useDispatch, useSelector } from 'react-redux';
 import { login, reset } from '../../store/authSlice';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import {
+  Container,
+  Box,
+  Typography,
+  TextField,
+  Button,
+  CircularProgress,
+  Alert,
+} from '@mui/material';
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -15,7 +24,7 @@ const Login = () => {
   const navigate = useNavigate();
 
   const { user, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth);
-  
+
   useEffect(() => {
     if (isError) {
       console.error(message);
@@ -27,7 +36,6 @@ const Login = () => {
 
     dispatch(reset());
   }, [user, isError, isSuccess, message, navigate, dispatch]);
-  
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -45,49 +53,111 @@ const Login = () => {
     dispatch(login(userData));
   };
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
   return (
-    <div>
-      <section className="heading">
-        <h1>Login</h1>
-      </section>
-
-      <section className="form">
-        <form onSubmit={onSubmit}>
-          <div className="form-group">
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              name="email"
-              value={email}
-              placeholder="Enter your email"
-              onChange={onChange}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              name="password"
-              value={password}
-              placeholder="Enter your password"
-              onChange={onChange}
-            />
-          </div>
-
-          <div className="form-group">
-            <button type="submit" className="btn btn-block">
+    <Container
+      component="main"
+      maxWidth="xs"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: '50vh',
+        background: '#3c3c3c',
+        borderRadius: "5px",
+      
+        boxShadow: 'rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px'
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography component="h1" variant="h5" sx={{ color: "#dda116", fontSize: "30px" }}>
+          Login
+        </Typography>
+        {isError && <Alert severity="error">{message}</Alert>}
+        <Box component="form" onSubmit={onSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={onChange}
+            sx={{
+              '& .MuiInputBase-root': {
+                color: '#fff',
+              },
+              '& .MuiFormLabel-root': {
+                color: '#ddd',
+              },
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  borderColor: '#ddd',
+                },
+                '&:hover fieldset': {
+                  borderColor: '#dda116',
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: '#dda116',
+                },
+              },
+            }}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={onChange}
+            sx={{
+              '& .MuiInputBase-root': {
+                color: "#fff",
+              },
+              '& .MuiFormLabel-root': {
+                color: '#ddd',
+              },
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  borderColor: '#ddd',
+                },
+                '&:hover fieldset': {
+                  borderColor: '#dda116',
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: '#dda116',
+                },
+              },
+            }}
+          />
+          {isLoading ? (
+            <CircularProgress style={{ color: '#dda116' }} />
+          ) : (
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2, bgcolor: '#dda116', '&:hover': { bgcolor: '#c8900d' } }}
+            >
               Submit
-            </button>
-          </div>
-        </form>
-      </section>
-    </div>
+            </Button>
+          )}
+        </Box>
+      </Box>
+    </Container>
   );
 };
 
