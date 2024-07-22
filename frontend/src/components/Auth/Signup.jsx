be3e10977232b1cf2ce2c67fc6563283f552b158
aniyant <kumarsunny30066@gmail.com>
2024-07-22 11:41:45 +0530

    register bugs fixed

--- frontend/src/components/Auth/Signup.jsx
+++ frontend/src/components/Auth/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { register, reset } from '../../store/authSlice';
-import { useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import './signup.css';
 import LeftComponent from '../../screens/Home/LeftComponent';
 
